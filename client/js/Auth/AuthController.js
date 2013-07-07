c060a1d320086aa39245a2101f9807f37ea45257
Alessandro Arpetti <arpetti@gmail.com>
2013-07-07 11:33:15 -0300

    js files organization

---
+++ client/js/Auth/AuthController.js
@@ -0,0 +1,94 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('ledita-app')
+.controller('AppCtrl', ['$scope', '$location', 'Auth', function($scope, $location, Auth) {
+    $scope.user = Auth.user;
+    $scope.userRoles = Auth.userRoles;
+    $scope.accessLevels = Auth.accessLevels;
+
+    $scope.logout = function() {
+        Auth.logout(function() {
+            $location.path('/login');
+        }, function() {
+            $rootScope.error = "Failed to logout";
+        });
+    };
+}]);
+
+angular.module('ledita-app')
+.controller('LoginCtrl',
+['$rootScope', '$scope', '$location', '$window', 'Auth', function($rootScope, $scope, $location, $window, Auth) {
+
+    $scope.rememberme = true;
+    $scope.login = function() {
+        Auth.login({
+                username: $scope.username,
+                password: $scope.password,
+                rememberme: $scope.rememberme
+            },
+            function(res) {
+                $location.path('/');
+            },
+            function(err) {
+                $rootScope.error = "Failed to login";
+            });
+    };
+
+    $scope.loginOauth = function(provider) {
+        $window.location.href = '/auth/' + provider;
+    };
+}]);
+
+angular.module('ledita-app')
+.controller('HomeCtrl',
+['$rootScope', function($rootScope) {
+
+}]);
+
+angular.module('ledita-app')
+.controller('RegisterCtrl',
+['$rootScope', '$scope', '$location', 'Auth', function($rootScope, $scope, $location, Auth) {
+    $scope.role = Auth.userRoles.user;
+    $scope.userRoles = Auth.userRoles;
+
+    $scope.register = function() {
+        Auth.register({
+                username: $scope.username,
+                password: $scope.password,
+                role: $scope.role
+            },
+            function() {
+                $scope.alertMsg = null; // clear the error message on successful registration
+                $location.path('/');
+            },
+            function(err) {
+                debugger;
+                $scope.alertMsg = err;
+            });
+    };
+}]);
+
+angular.module('ledita-app')
+.controller('PrivateCtrl',
+['$rootScope', function($rootScope) {
+}]);
+
+
+angular.module('ledita-app')
+.controller('AdminCtrl',
+['$rootScope', '$scope', 'Users', 'Auth', function($rootScope, $scope, Users, Auth) {
+    $scope.loading = true;
+    $scope.userRoles = Auth.userRoles;
+
+    Users.getAll(function(res) {
+        $scope.users = res;
+        $scope.loading = false;
+    }, function(err) {
+        $rootScope.error = "Failed to fetch users.";
+        $scope.loading = false;
+    });
+
+}]);
+
