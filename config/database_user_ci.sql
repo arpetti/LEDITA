438cafc56fa59a9ea74369c8915828cfb2e7f2a5
Daniela Baron <baron.daniela@gmail.com>
2013-07-12 23:35:04 -0400

    trying to fix travis permission error - give travis access to ledita db

--- config/database_user_ci.sql
+++ config/database_user_ci.sql
@@ -1,2 +1,3 @@
+GRANT ALL PRIVILEGES ON  `ledita-web-app`.* TO  'travis' WITH GRANT OPTION ;
 CREATE USER 'ledita' IDENTIFIED BY 'web*app01';
-GRANT SELECT ON  `ledita-web-app`.* TO  'ledita' ;
+GRANT SELECT ON  `ledita-web-app`.* TO  'ledita';
