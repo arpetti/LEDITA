0557c76745c3ef9271fd5550b3f6f568600af7f9
Daniela Baron <baron.daniela@gmail.com>
2013-07-15 19:28:05 -0400

    add connection pool init size to config, refactor js file name to Dao

--- server/dao/Dao.js
+++ server/dao/Dao.js
@@ -2,12 +2,12 @@ var configHelper = require('../../config/configHelper');
 var config = configHelper.config();
 var mysql = require('mysql');
 
-// TODO: Also configure pool size
 var pool  = mysql.createPool({
   host     : config.db_host,
   user     : config.db_user,
   password : config.db_pswd,
-  database : config.db_schema
+  database : config.db_schema,
+  connectionLimit : config.db_pool_connection_limit
 });
 
 module.exports = {
--- server/dao/LdDao.js
+++ server/dao/LdDao.js
@@ -1,5 +1,5 @@
 var mysql = require('mysql')
-  , dao = require('./ConnectPool.js');
+  , dao = require('./Dao.js');
 
 var QUERY_GET_LEARNING_DESIGNS = 'SELECT ld_id, ld_name, ld_scope, ld_publication, user_name, user_last_name from vw_ld_user';
 
