d45bebca5364f32f96d8f13c617ef3620a2f5124
Daniela Baron <baron.daniela@gmail.com>
2013-07-23 13:43:50 -0400

    add test for no user found by email

--- server/tests/dao/UserDaoSpec.js
+++ server/tests/dao/UserDaoSpec.js
@@ -5,9 +5,17 @@ describe('User DAO', function() {
 
 	it('Find existing user by email', function(done) {
         var email = 'sara@email.it';
-		UserDao.getUserByEmail(email, function(err, results){
-			expect(results).to.have.length(1);
+        UserDao.getUserByEmail(email, function(err, results){
+            expect(results).to.have.length(1);
             expect(results[0].email).to.equal(email);
+            done();
+        });
+    });
+
+    it('Looking for user that does not exist returns no rows', function(done) {
+        var email = 'nonExistingUser@test.com';
+		UserDao.getUserByEmail(email, function(err, results){
+			expect(results).to.have.length(0);
             done();
 		});
     });
