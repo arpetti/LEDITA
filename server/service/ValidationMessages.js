2d1492877773ab6d3f84f32d45b13eb0a8dc1fc9
Daniela Baron <baron.daniela@gmail.com>
2013-07-27 16:57:26 -0400

    fix first name length validation message

--- server/service/ValidationMessages.js
+++ server/service/ValidationMessages.js
@@ -1,2 +1,2 @@
-module.exports.FIRST_NAME_LENGTH = 'First name too short';
+module.exports.FIRST_NAME_LENGTH = 'First name must be 2-20 characters long';
 module.exports.SURNAME_LENGTH = 'Surname must be 2-20 characters long';
