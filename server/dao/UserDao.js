e7e8e5f6fa4e5b3d08638f5589ab0ef0c656e8f4
Daniela Baron <baron.daniela@gmail.com>
2013-08-02 19:49:17 -0400

    dao to get learning design by id

--- server/dao/UserDao.js
+++ server/dao/UserDao.js
@@ -1,16 +1,16 @@
 var mysql = require('mysql')
   , dao = require('./Dao.js');
 
-var QUERY_GET_USER_BY_EMAIL = 'SELECT id, name, last_name, gender, email, hash, workplace, city, country FROM user WHERE email = ?';
-var QUERY_GET_USER_BY_ID = 'SELECT id, name, last_name, gender, email, workplace, city, country FROM user WHERE id = ?';
+var GET_USER_BY_EMAIL = 'SELECT id, name, last_name, gender, email, hash, workplace, city, country FROM user WHERE email = ?';
+var GET_USER_BY_ID = 'SELECT id, name, last_name, gender, email, workplace, city, country FROM user WHERE id = ?';
 //TODO: Add constraint on user table to ensure email is unique
-var QUERY_ADD_USER = 'INSERT INTO user SET ?';
-var QUERY_DELETE_USER = 'DELETE FROM user WHERE ?';
+var ADD_USER = 'INSERT INTO user SET ?';
+var DELETE_USER = 'DELETE FROM user WHERE ?';
 
 module.exports = {
 
   getUserByEmail: function(email, callback) {
-    dao.findAll(QUERY_GET_USER_BY_EMAIL, [email], function(err, rows) {
+    dao.findAll(GET_USER_BY_EMAIL, [email], function(err, rows) {
       if (err) {
         callback(err);
         return;
@@ -20,7 +20,7 @@ module.exports = {
   },
 
   getUserById: function(id, callback) {
-    dao.findAll(QUERY_GET_USER_BY_ID, [id], function(err, rows) {
+    dao.findAll(GET_USER_BY_ID, [id], function(err, rows) {
       if (err) {
         callback(err);
         return;
@@ -30,7 +30,7 @@ module.exports = {
   },
 
   addUser: function(userJsonData, callback) {
-    dao.insertRecord(QUERY_ADD_USER, userJsonData, function(err, result) {
+    dao.insertRecord(ADD_USER, userJsonData, function(err, result) {
       if (err) {
         callback(err);
         return;
@@ -40,7 +40,7 @@ module.exports = {
   },
 
   deleteUser: function(userJsonData, callback) {
-    dao.insertRecord(QUERY_DELETE_USER, userJsonData, function(err, result) {
+    dao.insertRecord(DELETE_USER, userJsonData, function(err, result) {
       if (err) {
         callback(err);
         return;
--- server/tests/dao/LdDaoSpec.js
+++ server/tests/dao/LdDaoSpec.js
@@ -1,5 +1,6 @@
 var expect = require('chai').expect
-    , LdDao = require('../../dao/LdDao');
+    , LdDao = require('../../dao/LdDao')
+    , Dao = require('../../dao/Dao');
 
 describe('Learning Design DAO', function() {    
 
@@ -17,6 +18,27 @@ describe('Learning Design DAO', function() {
             expect(learningDesigns[0].ld_scope).to.equal('Lesson');
             expect(learningDesigns[0].user_name).to.equal('Mario');
             expect(learningDesigns[0].user_last_name).to.equal('Rossi');
+            done();
+        });
+    });
+
+    it('Get learning design by id 1 is LD Demo 1, created by user Mario Rossi', function(done) {
+        var learningDesignId = 1;
+        LdDao.getLearningDesign(learningDesignId, function(err, learningDesigns){
+            expect(learningDesigns).to.have.length(1);
+            expect(learningDesigns[0].ld_id).to.equal(1);
+            expect(learningDesigns[0].ld_name).to.equal('LD Demo 1');
+            expect(learningDesigns[0].ld_scope).to.equal('Lesson');
+            expect(learningDesigns[0].user_name).to.equal('Mario');
+            expect(learningDesigns[0].user_last_name).to.equal('Rossi');
+            done();
+        });
+    });
+
+    it('Get learning design for id that is not found returns no results', function(done) {
+        var learningDesignId = 99999;
+        LdDao.getLearningDesign(learningDesignId, function(err, learningDesigns){
+            expect(learningDesigns).to.have.length(0);
             done();
         });
     });
