41e9b61e898beebbfb5ccc0e134a356d85c09d16
Daniela Baron <baron.daniela@gmail.com>
2013-08-04 15:54:29 -0400

    refactor registration e2e tests for client side validation

--- test/client/e2e/register-scenarios.js
+++ test/client/e2e/register-scenarios.js
@@ -31,57 +31,56 @@ describe('Registration', function() {
 	    expect(element('#navBarUser', 'user nav is hidden after user logs out').css('display')).toBe('none');
 	});
 
-	// TODO: Nest a describe and have individual tests to verify each client side validation rule
-	it('Client side validation rules', function() {
-		var userNameToRegister = userNameGenerator.buildUserName + 'a';
+	describe('Client side validation', function() {
 
-		browser().navigateTo('/login');
-	    
-	    // First Name
-	    input('firstname').enter('John123');
-	    sleep(1);
-	    expect(element('#firstNameLetters').css('display')).toBe("inline");
-	    expect(element('#firstNameLetters').text()).toMatch('Your name can contain only letters');
-	    input('firstname').enter('John');
-	    sleep(1);
-	    expect(element('#firstNameLetters').css('display')).toBe("none");
-	    
-	    // Surname
-	    input('surname').enter('Smith');
-	    
-	    // Email (a.k.a. username)
-	    input('username').enter(userNameToRegister);
-	    
-	    // Password
-	    input('password').enter('1234567');
-	    sleep(1);
-	    expect(element('#passwordMinLength').css('display')).toBe("inline");
-	    expect(element('#passwordMinLength').text()).toMatch('Your password is required to be at least 8 characters');
-	    input('password').enter('12345678');
-	    sleep(1);
-	    expect(element('#passwordMinLength').css('display')).toBe("none");
-	    input('retypepassword').enter('12345678');
+		var verify = function(field, invalidInput, validInput, fieldErrorId, expectedErrorMessage) {
+			input(field).enter(invalidInput);
+		    sleep(1);
+		    expect(element('#signup').attr('disabled')).toBe("disabled");
+		    expect(element(fieldErrorId).css('display')).toBe("inline");
+		    expect(element(fieldErrorId).text()).toMatch(expectedErrorMessage);
+		    input(field).enter(validInput);
+		    sleep(1);
+		    expect(element(fieldErrorId).css('display')).toBe("none");
+		}
 
-	    input('terms').check();
-	    
-	    //TODO: Don't actually register here, just validate submit button is now enabled
-	    
-	    // element('#signup').click();
+		it('Invalid input is not allowed', function() {
+			browser().navigateTo('/login');
+			verify('firstname', 'John123', 'John', '#firstNameLetters', 'Your name can contain only letters');
+			verify('surname', 'Smith*&^%', 'Smith', '#surnameLetters', 'Your surname can contain only letters');
+			verify('username', 'john.smith.email.com', 'john.smith@email.com', '#emailFormat', 'Please, input a valid email');
+			verify('password', '1234567', '12345678', '#passwordMinLength', 'Your password is required to be at least 8 characters');
+		});
 
-	    // // Verify logged in view
-	    // expect(element('#navBarUser', 'user nav is displayed for logged in user').css('display')).toBe('block');
-	    // expect(element('#navBarAnon', 'anon nav is hidden for logged in user').css('display')).toBe('none');
-	    // expect(element('#loggedInUserName', 'user name is displayed for logged in user').text()).toBe(userNameToRegister);
+		it('Password and Retype Password must match', function() {
+			browser().navigateTo('/login');
+			input('password').enter('12345678');
+			input('retypepassword').enter('12345677');
+			sleep(1);
+			expect(element('#passwordNotMatch').css('display')).toBe("inline");
+			expect(element('#passwordNotMatch').text()).toMatch('Passwords do not match');
+			input('retypepassword').enter('12345678');
+			sleep(1);
+			expect(element('#passwordNotMatch').css('display')).toBe("none");
+		});
 
-	    // // Logout
-	    // element('#userActionsMenu').click();
-	    // element('#logoutLink').click();
+		it('Must accept terms and conditions', function() {
+			browser().navigateTo('/login');
+			input('firstname').enter('John');
+			input('surname').enter('Smith');
+			input('username').enter('john.smith@email.com');
+			input('password').enter('12345678');
+			input('retypepassword').enter('12345678');
+			sleep(1);
+			expect(element('#signup').attr('disabled')).toBe("disabled");
+			input('terms').check();
+			sleep(1);
+			expect(element('#signup').attr('disabled')).toBe(undefined);
+		});
 
-	    // // Verify anon view
-	    // expect(element('#navBarAnon', 'anon nav is displayed after user logs out').css('display')).toBe('block');
-	    // expect(element('#navBarUser', 'user nav is hidden after user logs out').css('display')).toBe('none');
 	});
 
+	//TODO: Nest this inside a describe 'Server side validation'
 	it('Cannot register a user that already exists', function() {
 		var existingUserName = testUsers.getUserName;
 
