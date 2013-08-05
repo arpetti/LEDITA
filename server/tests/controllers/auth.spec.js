81fc1f4e43cbe9d0e571c79a1d95cf060454ee83
Daniela Baron <baron.daniela@gmail.com>
2013-08-04 20:43:14 -0400

    more registerNewUser unit tests to cover error conditions

--- server/tests/controllers/auth.spec.js
+++ server/tests/controllers/auth.spec.js
@@ -153,6 +153,107 @@ describe('Auth controller', function() {
             AuthCtrl.registerNewUser(req, res, next);
         });
 
+        it('returns a 500 when password hash generation encounters unexpected error', function(done) {
+
+            var errorMessages = [];
+            var userValidateStub = sandbox.stub(UserValidator, "validate").returns(errorMessages);
+
+            var userValidateExistsStub = sandbox.stub(UserValidator, "validateExists", function(username, callback) {
+                callback(null, null);
+            });
+
+            var hashHelperStub = sandbox.stub(HashHelper, "generateHash", function(password, callback) {
+                callback(new Error("something went wrong"));
+            });
+            
+            var userServiceStub = sandbox.stub(UserService, "addNewUser");
+
+            res.send = function(httpStatus) {
+                expect(httpStatus).to.equal(500);
+                
+                assert.isTrue(userValidateStub.withArgs(req.body).calledOnce);
+                assert.isTrue(userValidateExistsStub.withArgs(req.body.username).calledOnce);
+                assert.isTrue(hashHelperStub.withArgs(req.body.password).calledOnce);
+                assert.equal(userServiceStub.callCount, 0, "user is not added when error occurs generating password hash");
+                done();
+            };
+
+            AuthCtrl.registerNewUser(req, res, next);
+        });
+
+        it('returns a 500 when adding a new user encounters unexpected error', function(done) {
+
+            var errorMessages = [];
+            var userValidateStub = sandbox.stub(UserValidator, "validate").returns(errorMessages);
+
+            var userValidateExistsStub = sandbox.stub(UserValidator, "validateExists", function(username, callback) {
+                callback(null, null);
+            });
+
+            var hashedPassword = "its-a-hash";
+            var hashHelperStub = sandbox.stub(HashHelper, "generateHash", function(password, callback) {
+                callback(null, hashedPassword);
+            });
+            
+            var userServiceStub = sandbox.stub(UserService, "addNewUser", function(user, hash, callback) {
+                callback(new Error("something went wrong"));
+            });
+
+            res.send = function(httpStatus) {
+                expect(httpStatus).to.equal(500);
+                
+                assert.isTrue(userValidateStub.withArgs(req.body).calledOnce);
+                assert.isTrue(userValidateExistsStub.withArgs(req.body.username).calledOnce);
+                assert.isTrue(hashHelperStub.withArgs(req.body.password).calledOnce);
+                assert.isTrue(userServiceStub.withArgs(req.body).calledOnce);
+                done();
+            };
+
+            AuthCtrl.registerNewUser(req, res, next);
+        });
+
+        it('calls next() with an error argument if req.logIn() returns error', function(done) {
+
+            var errorMessages = [];
+            var userValidateStub = sandbox.stub(UserValidator, "validate").returns(errorMessages);
+
+            var userValidateExistsStub = sandbox.stub(UserValidator, "validateExists", function(username, callback) {
+                callback(null, null);
+            });
+            
+            var hashedPassword = "its-a-hash";
+            var hashHelperStub = sandbox.stub(HashHelper, "generateHash", function(password, callback) {
+                callback(null, hashedPassword);
+            });
+
+            var addedUser = {
+                id: 999,
+                name: "John",
+                last_name: "Smith",
+                email: "john.smith@test.com",
+                role: {"bitMask":2,"title":"user"},
+                username: "john.smith@test.com"
+            };
+            var userServiceStub = sandbox.stub(UserService, "addNewUser", function(user, hash, callback) {
+                callback(null, addedUser);
+            });
+
+            req.logIn = function(user, callback) { return callback('SomeError'); };
+
+            next = function(err) {
+                expect(err).to.exist;
+
+                assert.isTrue(userValidateStub.withArgs(req.body).calledOnce);
+                assert.isTrue(userValidateExistsStub.withArgs(req.body.username).calledOnce);
+                assert.isTrue(hashHelperStub.withArgs(req.body.password).calledOnce);
+                assert.isTrue(userServiceStub.withArgs(req.body).calledOnce);
+                done();
+            };
+
+            AuthCtrl.registerNewUser(req, res, next);
+
+        });
+
     });
 
     describe('register()', function() {
