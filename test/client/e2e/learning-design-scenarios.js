0847eeb29f62eb688e6752bc9faea275e3e5e908
Daniela Baron <baron.daniela@gmail.com>
2013-08-17 10:45:56 -0400

    sort LDs by descending last edit date, add e2e test to verify ld detail page

--- test/client/e2e/learning-design-scenarios.js
+++ test/client/e2e/learning-design-scenarios.js
@@ -15,12 +15,37 @@ describe('Learning Design', function() {
     sleep(2);
 
     // Verify results
-    expect(repeater('.ld-item').count()).toBeGreaterThan(8);
+    expect(repeater('#ldlist .ld-item').count()).toBeGreaterThan(5);
 
     // Logout
     element('#userActionsMenu').click();
     element('#logoutLink').click();
 
+  });
+
+  it('Clicking on Learning Design displays detail view',function() {
+    var existingUserName = testUsers.getUserName;
+    var existingUserPassword = testUsers.getUserPassword;
+
+    browser().navigateTo('/login');
+    input('username').enter(existingUserName);
+    input('password').enter(existingUserPassword);
+    element('#loginButton').click();
+    sleep(2);
+
+    // Click on first result 
+    element('#ldlist .ld-item:nth-child(1) .ld-center a').click();
+    sleep(2);
+
+    // Verify detail view
+    // This is brittle because order by last edit date where all dates are same is unpredictable
+    expect(browser().location().url()).toBe('/ld/28');
+    expect(binding('learningDesign.ld_name')).toBe('Learning Design Title Demo 28');
+    expect(binding('learningDesign.ld_students_profile')).toBe('20 studenti adolescenti di livello B1');
+
+    // Logout
+    element('#userActionsMenu').click();
+    element('#logoutLink').click();
   });
 
 }); 
