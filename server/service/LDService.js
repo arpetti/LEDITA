ee73bfd5a6c4789f575d8cc19de6392aca080d5d
Daniela Baron <baron.daniela@gmail.com>
2013-08-18 10:36:58 -0400

    use when module to convert async calls to promises for LD Service

--- server/service/LDService.js
+++ server/service/LDService.js
@@ -1,32 +1,40 @@
 var LdDao = require('../dao/LdDao')
+    , when   = require('when')
+    , nodefn = require('when/node/function')
 	, messages = require('./ValidationMessages');
 
+/*
+    Using cujos/when module to convert async dao calls to promises,
+    to avoid the dreaded "christmas tree" code - deep nested async calls.
+    https://github.com/cujojs/when/blob/master/docs/api.md#node-style-asynchronous-functions
+*/
 module.exports = {
 
-	// TOOD: Use when fnlift to convert async's to promises and reduce xmas tree code!
-	// callback(err, message, learningDesignDetail)
-	getLearningDesignDetail: function(ldid, callback) {
-		var learningDesignDetail = {};
-		LdDao.getLearningDesign(ldid, function(err, learningDesign) {
-            if(err) {
-                callback(err);
+    // callback(err, message, learningDesignDetail)
+    getLearningDesignDetail: function(ldid, callback) {
+        var learningDesignDetail = {};
+        
+        var promiseDaoGetLd = nodefn.call(LdDao.getLearningDesign, ldid);
+        var promiseDaoGetLdSubjects = nodefn.call(LdDao.getLearningDesignSubjects, ldid);
+
+        promiseDaoGetLd.then(function(results) {
+            if (results.length === 0) {
+                callback(null, messages.LD_NOT_FOUND, null);
                 return;
-            } else { 
-                if (learningDesign.length === 0) {
-                    callback(null, messages.LD_NOT_FOUND, null);
-                    return;
-                }
-                learningDesignDetail = learningDesign[0];
-                LdDao.getLearningDesignSubjects(ldid, function(err, results) {
-                	if(err) {
-                		callback(err);
-                		return;
-                	} else {
-                		learningDesignDetail.subjects = results;
-                		callback(null, null, learningDesignDetail);
-                	}
-                });
             }
+            learningDesignDetail = results[0];
+        }, function(err) {
+            callback(err);
+            return;
         });
-	}
+
+        promiseDaoGetLdSubjects.then(function(results) {
+            learningDesignDetail.subjects = results;
+            callback(null, null, learningDesignDetail);
+        }, function(err) {
+            callback(err);
+            return;
+        });
+
+    }
 };
