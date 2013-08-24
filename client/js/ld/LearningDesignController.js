5bb19589665c4bb7f409cbd54b0b5f2bd855a241
Daniela Baron <baron.daniela@gmail.com>
2013-08-23 22:07:36 -0400

    introduce mock data for activity tree

--- client/js/ld/LearningDesignController.js
+++ client/js/ld/LearningDesignController.js
@@ -1,6 +1,6 @@
 angular.module('ledita-app')
 .controller('LdViewCtrl',
-['$scope', '$routeParams', 'LDService', function($scope, $routeParams, LDService) {
+['$scope', '$routeParams', 'LDService', '$http', function($scope, $routeParams, LDService, $http) {
 
 	$scope.ldid = $routeParams.ldid;
 
@@ -9,5 +9,24 @@ angular.module('ledita-app')
     }, function(err) {
         $scope.error = "Failed to fetch learning design.";
     });
+
+    $http.get('/mockdata/ldactivity1.json').success(function(data) {
+    	console.log(JSON.stringify(data));
+    	$scope.levels = data;
+  	}); 
+
+  	$scope.getBoxClass = function (node) {
+  		if (node.children) {
+  			if (node.children.length >= 4)
+  				return "groupBox4";
+  			if (node.children.length >= 3)
+  				return "groupBox3";
+  			if (node.children.length >= 2)
+  				return "groupBox1";
+  			if (node.children.lenght >= 1)
+  				return "groupBox1";
+  		}
+  		return "actBox";
+    };  
 
 }]);
