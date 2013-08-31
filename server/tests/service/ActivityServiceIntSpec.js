39685f56e00b554cb05c6ba9558bf96211337232
Daniela Baron <baron.daniela@gmail.com>
2013-08-31 12:57:02 -0400

    activity service integration test

--- server/tests/service/ActivityServiceIntSpec.js
+++ server/tests/service/ActivityServiceIntSpec.js
@@ -1,1 +1,35 @@
 // This is an integration test because the dependencies are not mocked out
+var expect = require('chai').expect;
+var ActivityService = require('../../service/ActivityService');
+var messages = require('../../service/ValidationMessages');
+var _ = require('underscore');
+
+describe('Activity Service Integration', function() {
+
+	it('Returns results for Learning Design 2', function(done) {
+
+		var ldid = 2;
+		ActivityService.getLDActivityStructure(ldid, function(err, result, message){
+			expect(err).to.be.null;
+            expect(message).to.be.null;
+
+            expect(_.keys(result)).to.have.length(4); // 4 levels
+            
+            expect(result[1]).to.have.length(2); // Level 1 has 2 Nodes
+            expect(result[1][0].type).to.equal('ACTIVITY_GROUP'); // Level 1 first node is activity group
+            expect(result[1][0].node_name).to.be.null; // Level 1 first node activity group has no name
+            expect(_.keys(result[1][0].children)).to.have.length(2); // Level 1 first node activity group has 2 levels of children
+            expect(result[1][0].children[1]).to.have.length(1); // First level of children has 1 activity
+            expect(result[1][0].children[1][0].activity_name).to.equal('Learning Activity 5');	
+            expect(result[1][0].children[2]).to.have.length(1); // Second level of children has 1 activity
+            expect(result[1][0].children[2][0].activity_name).to.equal('Learning Activity 6');	
+
+            expect(result[1][1].type).to.equal('ACTIVITY_GROUP'); // Level 1 second node is activity group
+            expect(result[1][1].node_name).to.equal('Group 3 Name'); // Level 1 second node activity group name
+
+			done();
+		});
+
+	});
+
+});
--- server/tests/service/LDServiceIntSpec.js
+++ server/tests/service/LDServiceIntSpec.js
@@ -9,39 +9,32 @@ describe('LD Service Integration', function() {
 	it('Gets a learning design with subjects, objectives, prerequisites', function(done) {
 		var learningDesignId = 1;
 		LDService.getLearningDesignPromise(learningDesignId).then(function(result) {
-			expect(result).to.have.length(LDService.LD_NUMBER_OF_DATA_ELEMENTS);
-			
-			var learningDesign = result[0];
-			expect(learningDesign).to.have.length(1);
-			learningDesignObj = learningDesign[0];
-			expect(learningDesignObj.ld_id).to.equal(1);
-            expect(learningDesignObj.ld_name).to.equal('Learning Design Title Demo 1');
-            expect(learningDesignObj.ld_scope).to.equal('Lesson');
-            expect(learningDesignObj.ld_students_profile).to.equal('20 studenti adolescenti di livello B1');
-            expect(learningDesignObj.user_name).to.equal('Mario');
-            expect(learningDesignObj.user_last_name).to.equal('Rossi');
-            expect(learningDesignObj.ld_creation_date).not.to.be.null;
-            expect(learningDesignObj.ld_last_edit_date).not.to.be.null;
+      		expect(result).to.have.length(LDService.LD_NUMBER_OF_DATA_ELEMENTS);
+      		
+      		var learningDesign = result[0];
+      		expect(learningDesign).to.have.length(1);
+      		learningDesignObj = learningDesign[0];
+      		expect(learningDesignObj.ld_id).to.equal(1);
+                  expect(learningDesignObj.ld_name).to.equal('Learning Design Title Demo 1');
+                  expect(learningDesignObj.ld_scope).to.equal('Lesson');
+                  expect(learningDesignObj.ld_students_profile).to.equal('20 studenti adolescenti di livello B1');
+                  expect(learningDesignObj.user_name).to.equal('Mario');
+                  expect(learningDesignObj.user_last_name).to.equal('Rossi');
+                  expect(learningDesignObj.ld_creation_date).not.to.be.null;
+                  expect(learningDesignObj.ld_last_edit_date).not.to.be.null;
 
-            var subjects = result[1];
-            expect(subjects).to.have.length(2);
-            expect(subjects[0].subject_name).to.equal('Topic 1');
-            expect(subjects[1].subject_name).to.equal('Topic 5');
+                  var subjects = result[1];
+                  expect(subjects).to.have.length(2);
+                  expect(subjects[0].subject_name).to.equal('Topic 1');
+                  expect(subjects[1].subject_name).to.equal('Topic 5');
 
-            var objectives = result[2];
-            expect(objectives).to.have.length(2);
-            expect(objectives[0].objective_descr).to.equal('Objective 1');
-            expect(objectives[1].objective_descr).to.equal('Objective 6');
+                  var objectives = result[2];
+                  expect(objectives).to.have.length(2);
+                  expect(objectives[0].objective_descr).to.equal('Objective 1');
+                  expect(objectives[1].objective_descr).to.equal('Objective 6');
 
-            var prerequisites = result[3];
-            expect(prerequisites).to.have.length(2);
-
-            /*
-            expect(results[0].prereq_name).to.equal('Objective 1');
-            expect(results[0].prereq_type).to.equal('OBJECTIVE');
-            expect(results[1].prereq_name).to.equal('Objective 2');
-            expect(results[1].prereq_type).to.equal('OBJECTIVE');
-            */
+                  var prerequisites = result[3];
+                  expect(prerequisites).to.have.length(2);
 		}).then(done, done);
 	});
 
