ba5a31bed7a93f8644024ab28f5856179f9668c2
Daniela Baron <baron.daniela@gmail.com>
2013-09-02 09:53:25 -0400

    work in progress on run all tests script

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -1,21 +1,50 @@
-# Work in progress, do not run yet
+#!/bin/bash
 
-# Server should be stopped before running this
+#####################################################################################
+#  
+# 	This script is for local development only. It runs all tests including:
+#
+#  	 * server side unit, dao, and integration tests
+#  	 * client side unit tests
+#	 * client side end to end tests
+#
+#	The MySQL database must be running for this to work.
+#
+######################################################################################
 
-# Run server side unit, dao, and integration tests
+mysqlpid=`ps -ax | pgrep mysql`
+set -- $mysqlpid
+if [[ -z "$mysqlpid" ]]; then
+	echo "Please start mysql then run this script again, goodbye."
+	exit 1
+fi
+
+nodepid=`ps -ax | pgrep node`
+set -- $nodepid
+if [[ -n "$nodepid" ]]; then
+	echo "Node web server already running at pid: $nodepid, killing..."
+	kill $nodepid
+	sleep 2
+fi
+
+echo "Running server side unit, dao, and integration tests..."
 npm test
+serverTestStatus=$?
 
-# Run client side Angular-Karma unit tests
+echo "Running client side unit tests..."
 npm run-script client-test
+clientUnitTestStatus=$?
+
+echo "Server test status is $serverTestStatus, Client test status is $clientUnitTestStatus"
 
 # FIXME How to check if server already started?
 # Start server
-NODE_ENV=dev node server.js > /dev/null &
-sleep 3
+#NODE_ENV=dev node server.js > /dev/null &
+#sleep 2
 
 # TODO Investigate if possible to define e2e tests in package.json similar to client-test
 # Run client side Angular-Karma end-to-end tests
-test/client/scripts/local-e2e-test.sh
+#test/client/scripts/local-e2e-test.sh
 
 # TODO Kill Server, but NODE, not RUBY
 # http://stackoverflow.com/questions/6437602/shell-script-to-get-the-process-id-on-linux
