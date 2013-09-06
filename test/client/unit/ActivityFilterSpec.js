42d0eab13ca969f765f0e955ed329236977d4074
Daniela Baron <baron.daniela@gmail.com>
2013-09-05 20:20:12 -0400

    more unit tests for duration filter

--- test/client/unit/ActivityFilterSpec.js
+++ test/client/unit/ActivityFilterSpec.js
@@ -6,10 +6,40 @@ describe('Activity Duration Display', function() {
 
         describe('Activity Duration', function() {
 
-            it('Only shows minutes', inject(function(durationDisplayFilter) {
+            it('Displays minutes', inject(function(durationDisplayFilter) {
                 var node = {"dur_min": 15};
                 expect(durationDisplayFilter(node)).toBe('15min');
-             }));
+            }));
+
+            it('Displays hours', inject(function(durationDisplayFilter) {
+                var node = {"dur_hh": 2};
+                expect(durationDisplayFilter(node)).toBe('2hh');
+            }));
+
+            it('Displays days', inject(function(durationDisplayFilter) {
+                var node = {"dur_dd": 10};
+                expect(durationDisplayFilter(node)).toBe('10day');
+            }));
+
+            it('Displays months', inject(function(durationDisplayFilter) {
+                var node = {"dur_mon": 3};
+                expect(durationDisplayFilter(node)).toBe('3mon');
+            }));
+
+            it('Displays minutes and hours', inject(function(durationDisplayFilter) {
+                var node = {"dur_min": 15, "dur_hh": 3};
+                expect(durationDisplayFilter(node)).toBe('15min 3hh');
+            }));
+
+            it('Does not show 0 units', inject(function(durationDisplayFilter) {
+                var node = {"dur_min": 15, "dur_hh": 0, "dur_dd": 5};
+                expect(durationDisplayFilter(node)).toBe('15min 5day');
+            }));
+
+            it('Does not show anything if all units are 0', inject(function(durationDisplayFilter) {
+                var node = {"dur_min": 0, "dur_hh": 0, "dur_dd": 0, "dur_mon": 0};
+                expect(durationDisplayFilter(node)).toBe('');
+            }));
 
         });
 });
