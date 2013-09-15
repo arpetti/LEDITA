7d0d7308daf2af31237e0cada7d155a3ffdba009
Daniela Baron <baron.daniela@gmail.com>
2013-09-15 18:54:55 -0400

    #3 add subject wildcard search in preparation for typeahead

--- server/dao/LdCreateDao.js
+++ server/dao/LdCreateDao.js
@@ -4,6 +4,12 @@ var dao = require('./Dao.js');
 var CREATE_LD = 'INSERT INTO ld SET ?';
 var DELETE_USER = 'DELETE FROM ld WHERE ?';
 
+var GET_SUBJECTS_MATCHING = "select name from subject where name like ?";
+
+var addWildCard = function(partial) {
+	return '%' + partial + '%';
+}
+
 module.exports = {
 
 	createLd: function(ldData, callback) {
@@ -24,6 +30,16 @@ module.exports = {
 		    	callback(null, null);
 	      	}
 	    });
+	},
+
+	getSubjectsMatching: function(partial, callback) {
+		dao.findAll(GET_SUBJECTS_MATCHING, [addWildCard(partial)], function(err, results) {
+			if (err) {
+				callback(err);
+			} else {
+				callback(null, results);
+			}
+		});
 	}
 
 };
