21fa63b5d716b113217a50692b76eea45b78c289
Daniela Baron <baron.daniela@gmail.com>
2013-09-15 14:59:23 -0400

    introduce dao to handle ld creation and happy path test

---
+++ server/dao/LdCreateDao.js
@@ -0,0 +1,29 @@
+var mysql = require('mysql')
+var dao = require('./Dao.js');
+
+var CREATE_LD = 'INSERT INTO ld SET ?';
+var DELETE_USER = 'DELETE FROM ld WHERE ?';
+
+module.exports = {
+
+	createLd: function(ldData, callback) {
+		dao.insertRecord(CREATE_LD, ldData, function(err, ldid) {
+			if(err) {
+				callback(err);
+			} else {
+				callback(null, ldid);
+			}
+		});
+	},
+
+	deleteLd: function(userJsonData, callback) {
+	    dao.deleteRecord(DELETE_USER, userJsonData, function(err, result) {
+	    	if (err) {
+	        	callback(err);
+	      	} else {
+		    	callback(null, null);
+	      	}
+	    });
+	}
+
+};
