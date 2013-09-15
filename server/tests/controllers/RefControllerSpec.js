04fa4df5db8b941ef4acd9aa4e8f7960354b722e
Daniela Baron <baron.daniela@gmail.com>
2013-09-15 19:48:19 -0400

    unit tests for ref controller get subjects

--- server/tests/controllers/RefControllerSpec.js
+++ server/tests/controllers/RefControllerSpec.js
@@ -10,17 +10,17 @@ describe('Reference Data Controller', function() {
         , res = {}
         , sandbox = sinon.sandbox.create();
 
-    beforeEach(function() {
-
-    });
-
-    afterEach(function() {
-        sandbox.restore();
-    });
 
     describe('getQcers', function() {
+    
+	    beforeEach(function() {
+	    });
 
-    	it('Returns a 200 with results from Reference Service', function(done) {
+	    afterEach(function() {
+	        sandbox.restore();
+	    });
+
+    	it('Returns a 200 with Qcer results from Reference Service', function(done) {
 
     		var qcers = [{"qcer_id": 1, "qcer_name": "Q1"}, {"qcer_id": 2, "qcer_name": "Q2"}];
     		var serviceStub = sandbox.stub(RefService, "getQcers", function(callback) {
@@ -76,5 +76,52 @@ describe('Reference Data Controller', function() {
     	});
 
     });
+
+	describe('getSubjectsMatching', function() {
+
+		beforeEach(function() {
+	    });
+
+	    afterEach(function() {
+	        sandbox.restore();
+	    });
+
+	    it('Returns a 200 with Subject results from Reference Service', function(done) {
+
+	    	var subjects = [{"name":"Topic 1"},{"name":"Topic 2"}];
+    		var serviceStub = sandbox.stub(RefService, "getSubjectsMatching", function(callback) {
+                callback(null, subjects, null);
+            });
+
+            res.json = function(httpStatus, result) {
+                expect(httpStatus).to.equal(200);
+                expect(result).to.equal(subjects);
+                
+                assert.isTrue(serviceStub.calledOnce);
+                done();
+            };
+
+            RefController.getSubjectsMatching(req, res);
+	    });
+
+	    it('Returns a 500 with message if Reference Service calls back with error', function(done) {
+
+    		var error = new Error("something went wrong");
+    		var refMessage = "Unable to retrieve subjects";
+    		var serviceStub = sandbox.stub(RefService, "getSubjectsMatching", function(callback) {
+                callback(error, null, refMessage);
+            });
+
+            res.send = function(httpStatus, message) {
+                expect(httpStatus).to.equal(500);
+                expect(message).to.equal(refMessage);
+                
+                assert.isTrue(serviceStub.calledOnce);
+                done();
+            };
+
+            RefController.getSubjectsMatching(req, res);
+    	});
+	});
 
 });    
