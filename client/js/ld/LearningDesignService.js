39a73b7941c9c3699f9116250037c2bbdb70b9b8
Alessandro <arpetti@gmail.com>
2013-09-22 17:00:26 -0300

    add ld as a part modal

--- client/js/ld/LearningDesignService.js
+++ client/js/ld/LearningDesignService.js
@@ -12,6 +12,14 @@ angular.module('ledita-app')
         	$http.get(activityUrl).success(success).error(error);
         },
 
+        getLearningDesigns: function(success, error) {
+            $http.get('/learningDesigns').success(success).error(error);
+        },
+
+        getQcers: function(success, error) {
+            $http.get('/reference/qcer').success(success).error(error);
+        },
+
         getBoxClass: function(node) {
             if (node.type === 'ACTIVITY') {
                 return 'actBox';
