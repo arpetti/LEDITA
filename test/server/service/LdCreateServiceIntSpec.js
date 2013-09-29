bb77339006ab8609370fb39596d8ff2d7e16b128
Daniela Baron <baron.daniela@gmail.com>
2013-09-29 18:50:07 -0400

    #26 add integration test for new scope created

--- test/server/service/LdCreateServiceIntSpec.js
+++ test/server/service/LdCreateServiceIntSpec.js
@@ -11,7 +11,9 @@ var _ = require('underscore');
 // This is an integration test because the dependencies are not mocked out
 describe('LD Create Service Integration', function() {
 
+	var numExistingLds = 31; // known from demo data
 	var ldName = "LD Created From Integration Test";
+	var newScopeName = "Seminar New Integration Test";
 	var existingTopicName = "Topic 1";
 	var newTopicName = "New Topic From Integration Test";
 	var existingObjective = "Objective 1";
@@ -29,6 +31,7 @@ describe('LD Create Service Integration', function() {
 	var cleanupNeeds = 'DELETE FROM needs where ld_id = (select id from ld where ?)';
 	var cleanupObjectives = 'DELETE FROM objective where descr = ?';
 	var cleanupPrerequisites = 'DELETE FROM objective where descr = ?';
+	var cleanupScope = 'DELETE FROM scope where name = ?';
 
 	var verifyLdQuery = 'select id, user_id, ld_model_id, name, scope_id, publication, students_profile, creation_date, last_edit_date from ld where id = ?';
 	var verifyLdQcerQuery = 'select ld_id, qcer_name from vw_ld_qcer where ld_id = ? order by qcer_name';
@@ -37,6 +40,7 @@ describe('LD Create Service Integration', function() {
 	var verifyLdObjectiveQuery = 'select ld_name, objective_descr from vw_ld_objective where ld_id = ?';
 	var verifyLdPrerequisiteQuery = 'select ld_name, prereq_name, prereq_type from vw_ld_prerequisite where ld_id = ?';
 	var verifyObjectiveQuery = 'select id, descr from objective where descr = ?';
+	var verifyScopeQuery = 'select id, name from scope where name = ?';
 
 	afterEach(function(done) {
 		async.series([
@@ -79,6 +83,11 @@ describe('LD Create Service Integration', function() {
 		        Dao.deleteRecord(cleanupLd, cleanupData, function(err, results) {
 		        	callback(null, null);
 		        })
+		    },
+		    function(callback){
+		    	Dao.deleteRecord(cleanupScope, [newScopeName], function(err, results) {
+		    		callback(null, null);
+		    	});
 		    }
 		],
 		function(err, results ) {
@@ -88,7 +97,6 @@ describe('LD Create Service Integration', function() {
 
 	// TODO: Add another integration test where new scope is created
 	it('Creates LD, child entities, and associations', function(done) {
-		var numExistingLds = 31; // known from demo data
 		var userId = 4;
 		var qcer3Name = 'B1'; // known from demo data
 		var qcer6Name = 'C2'; // known from demo data
@@ -170,6 +178,53 @@ describe('LD Create Service Integration', function() {
 				});
     		});
     	});
+	});
+
+	it('Creates a new scope if not already existing', function(done) {
+		var qcer3Name = 'B1'; // known from demo data
+		var qcer6Name = 'C2'; // known from demo data
+		var userId = 4;
+		var ldData = {
+    		name: ldName,
+    		qcers: {"3": true, "6": true},
+    		scope: newScopeName,
+    		topics: [existingTopicName, newTopicName],
+    		objectives: [existingObjective, newObjective],
+    		requisites: [existingRequisite, newRequisite],
+    		studentsDescription: "Students Description From Integration Test"
+    	};
+    	var today = new Date();
+
+    	LDCreateService.createLd(userId, ldData, function(err, ldid, message) {
+    		expect(err).to.be.null;
+    		expect(message).to.be.null;
+    		expect(ldid).not.to.be.null;
+    		expect(ldid).to.be.above(numExistingLds);
+    		
+    		// Verify newly created LD
+    		Dao.findAll(verifyLdQuery, [ldid], function(err, results) {
+    			var newScopeId = results[0].scope_id;
+    			expect(results).to.have.length(1);
+    			expect(results[0].id).to.equal(ldid);
+    			expect(results[0].user_id).to.equal(userId);
+    			expect(results[0].ld_model_id).to.be.null;
+    			expect(results[0].name).to.equal(ldData.name);
+    			expect(results[0].scope_id).to.be.above(4); // known from demo data
+    			expect(results[0].publication).to.equal(0);
+    			expect(results[0].students_profile).to.equal(ldData.studentsDescription);
+    			expect(results[0].creation_date).to.equalDate(today);
+				expect(results[0].last_edit_date).to.equalDate(today);
+
+				// Verify newly created Scope
+				Dao.findAll(verifyScopeQuery, [newScopeName], function(err, results) {
+					expect(results).to.have.length(1);
+					expect(results[0].id).to.equal(newScopeId);
+					expect(results[0].name).to.equal(newScopeName);
+					done();
+				})
+			});
+		});
+
 	});
 
 });
