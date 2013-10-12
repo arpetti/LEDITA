930d49762949c93e02270c6da671cff3057cc81a
Daniela Baron <baron.daniela@gmail.com>
2013-10-12 14:26:22 -0400

    fix dao test to reset last edit date, otherwise breaks e2e test on travis

--- test/server/dao/LdEditDaoSpec.js
+++ test/server/dao/LdEditDaoSpec.js
@@ -8,15 +8,17 @@ var Dao = require('../../../server/dao/Dao');
 describe('LD Edit', function () {
 
 	var ldIdToEdit = 27;
+	
+	var ldOriginalLastEditDate = new Date('2013-01-11 22:21:26');
+	var ldOriginalName = 'Learning Design Title Demo 27';
+	
 	var verifyLd = 'SELECT id, name, publication, students_profile, last_edit_date FROM ld where id = ?';
+	var resetLd = 'UPDATE ld SET name = ?, last_edit_date = ? WHERE id = ?';
 
 	describe('Update LD Name', function() {
 
-		var ldOriginalName = 'Learning Design Title Demo 27';
-
 		afterEach(function(done) {
-			var resetLd = 'UPDATE ld SET name = ? WHERE id = ?';
-			Dao.insertRecord(resetLd, [ldOriginalName, ldIdToEdit], function(err, result) {
+			Dao.insertRecord(resetLd, [ldOriginalName, ldOriginalLastEditDate, ldIdToEdit], function(err, result) {
 				done();
 			});
 		});
