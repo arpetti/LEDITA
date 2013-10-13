2ef202afc773f918070d20ffd889790c4965a6d7
Daniela Baron <baron.daniela@gmail.com>
2013-10-13 08:32:47 -0400

    #28 unit tests for ld edit service update ld name

--- server/service/LdEditService.js
+++ server/service/LdEditService.js
@@ -3,7 +3,6 @@ var messages = require('../validate/ValidationMessages');
 
 module.exports = {
 
-	// #28 wip, still needs unit tests
 	// callback(err, message)
 	updateLdName: function(ldName, ldId, callback) {
 		LdEditDao.updateLdName(ldName, ldId, function(err, result) {
---
+++ test/server/service/LdEditServiceSpec.js
@@ -0,0 +1,64 @@
+var expect = require('chai').expect;
+var assert = require('chai').assert;
+var sinon = require('sinon');
+var LdEditService = require('../../../server/service/LdEditService');
+var LdEditDao = require('../../../server/dao/LdEditDao');
+var messages = require('../../../server/validate/ValidationMessages');
+
+describe('Learning Design Edit Service', function() {
+
+	var sandbox = sinon.sandbox.create();
+
+    beforeEach(function() {
+
+    });
+
+    afterEach(function() {
+        sandbox.restore();
+    });
+
+	describe('Update Learning Design Name', function() {
+
+		it('Calls back with error and message if dao fails', function(done) {
+			var ldName = 'a modified name';
+			var ldId = 387;
+
+			var daoError = new Error('something went wrong');
+	        var daoStub = sandbox.stub(LdEditDao, "updateLdName", function(ldName, ldId, callback) {
+	            callback(daoError);
+	        });
+
+	        var serviceCB = function(err, message) {
+	        	expect(err).not.to.be.null;
+	        	expect(err).to.equal(daoError);
+	        	expect(message).to.equal(messages.LD_NAME_UPDATE_FAIL);
+
+	        	assert.isTrue(daoStub.withArgs(ldName, ldId).calledOnce);
+	        	done();
+	        };
+
+	        LdEditService.updateLdName(ldName, ldId, serviceCB);
+		});
+
+		it('Calls back with nothing if dao update successful', function(done) {
+			var ldName = 'a modified name';
+			var ldId = 387;
+
+	        var daoStub = sandbox.stub(LdEditDao, "updateLdName", function(ldName, ldId, callback) {
+	            callback();
+	        });
+
+	        var serviceCB = function(err, message) {
+	        	expect(err).to.be.undefined;
+	        	expect(message).to.be.undefined;
+
+	        	assert.isTrue(daoStub.withArgs(ldName, ldId).calledOnce);
+	        	done();
+	        };
+
+	        LdEditService.updateLdName(ldName, ldId, serviceCB);
+		});
+
+	});
+
+});
