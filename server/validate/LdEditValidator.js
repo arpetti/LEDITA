a4232a2561af4e1a6201b26961f9a15461b6b793
Daniela Baron <baron.daniela@gmail.com>
2013-10-13 20:47:23 -0400

    #28 validator for students description

--- server/validate/LdEditValidator.js
+++ server/validate/LdEditValidator.js
@@ -6,12 +6,19 @@ module.exports = {
 
 	validateLdName: function(ldName) {
 		var em = [];
-
 		em.push(vh.validateAny(vh.validateNullEmpty, [ldName, m.LD_NAME_REQUIRED]));
 		em.push(vh.validateAny(vh.validateLength, [ldName, m.LD_NAME_LENGTH, 1, 50]));
 		em.push(vh.validateAny(vh.validateRegEx, 
 			[ldName, m.LD_NAME_ALLOWED_CHARS, vh.FIRST_ALPHANUMERIC_REST_ANY_CHAR]));
+		return _.filter(em, function(message){ return message !== null; });
+	},
 
+	validateStudentsDescr: function(studentsDescr) {
+		var em = [];
+		em.push(vh.validateAny(vh.validateNullEmpty, [studentsDescr, m.LD_STUDENTS_DESC_REQUIRED]));
+		em.push(vh.validateAny(vh.validateLength, [studentsDescr, m.LD_STUDENTS_DESC_LENGTH, 1, 500]));
+		em.push(vh.validateAny(vh.validateRegEx, 
+			[studentsDescr, m.LD_STUDENTS_DESC_ALLOWED_CHARS, vh.FIRST_ALPHANUMERIC_REST_ANY_CHAR]));
 		return _.filter(em, function(message){ return message !== null; });
 	}
 };
--- test/server/validate/LdEditValidatorSpec.js
+++ test/server/validate/LdEditValidatorSpec.js
@@ -13,6 +13,19 @@ describe('LD Edit Validation', function() {
 			expect(results).to.have.length(0);
 		});
 
+		it('Can be exactly 50 characters long', function() {
+			var ldName = TestUtils.buildString(50, 'a');
+			var results = LdEditValidator.validateLdName(ldName);
+			expect(results).to.have.length(0);
+		});
+
+		it('Cannot exceed 50 characters', function() {
+			var ldName = TestUtils.buildString(51, 'a');
+			var results = LdEditValidator.validateLdName(ldName);
+			expect(results).to.have.length(1);
+			expect(results[0]).to.equal(messages.LD_NAME_LENGTH);
+		});
+
 		it('Cannot be empty', function() {
 			var ldName = "";
 			var results = LdEditValidator.validateLdName(ldName);
@@ -44,6 +57,62 @@ describe('LD Edit Validation', function() {
 			var results = LdEditValidator.validateLdName(ldName);
 			expect(results).to.have.length(1);
 			expect(results[0]).to.equal(messages.LD_NAME_ALLOWED_CHARS);
+		});
+
+	});
+
+	describe('Students Description', function() {
+
+		it('Can contain letters, numbers and some special chars', function() {
+			var studentsDescr = "123 ABC * - _ + & #";
+			var results = LdEditValidator.validateStudentsDescr(studentsDescr);
+			expect(results).to.have.length(0);
+		});
+
+		it('Can be exactly 500 characters long', function() {
+			var studentsDescr = TestUtils.buildString(500, 'b');
+			var results = LdEditValidator.validateStudentsDescr(studentsDescr);
+			expect(results).to.have.length(0);
+		});
+
+		it('Cannot exceed 500 characters', function() {
+			var studentsDescr = TestUtils.buildString(501, 'a');
+			var results = LdEditValidator.validateStudentsDescr(studentsDescr);
+			expect(results).to.have.length(1);
+			expect(results[0]).to.equal(messages.LD_STUDENTS_DESC_LENGTH);
+		});
+
+		it('Cannot be empty', function() {
+			var studentsDescr = "";
+			var results = LdEditValidator.validateStudentsDescr(studentsDescr);
+			expect(results).to.have.length(3);
+			expect(results[0]).to.equal(messages.LD_STUDENTS_DESC_REQUIRED);
+			expect(results[1]).to.equal(messages.LD_STUDENTS_DESC_LENGTH);
+			expect(results[2]).to.equal(messages.LD_STUDENTS_DESC_ALLOWED_CHARS);
+		});
+
+		it('Cannot be null', function() {
+			var studentsDescr = null;
+			var results = LdEditValidator.validateStudentsDescr(studentsDescr);
+			expect(results).to.have.length(3);
+			expect(results[0]).to.equal(messages.LD_STUDENTS_DESC_REQUIRED);
+			expect(results[1]).to.equal(messages.LD_STUDENTS_DESC_LENGTH);
+			expect(results[2]).to.equal(messages.LD_STUDENTS_DESC_ALLOWED_CHARS);
+		});
+
+		it('Cannot be undefined', function() {
+			var results = LdEditValidator.validateStudentsDescr();
+			expect(results).to.have.length(3);
+			expect(results[0]).to.equal(messages.LD_STUDENTS_DESC_REQUIRED);
+			expect(results[1]).to.equal(messages.LD_STUDENTS_DESC_LENGTH);
+			expect(results[2]).to.equal(messages.LD_STUDENTS_DESC_ALLOWED_CHARS);
+		});
+
+		it('Cannot contain script tags', function() {
+			var studentsDescr = '<script>window.location("evil.com");</script>'
+			var results = LdEditValidator.validateStudentsDescr(studentsDescr);
+			expect(results).to.have.length(1);
+			expect(results[0]).to.equal(messages.LD_STUDENTS_DESC_ALLOWED_CHARS);
 		});
 
 	});
