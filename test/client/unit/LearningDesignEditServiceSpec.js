f851e6b9355101f7f41efd60174346b9819094b8
Daniela Baron <baron.daniela@gmail.com>
2013-10-15 15:53:37 -0400

    #28 more unit tests for qcer mapping

--- test/client/unit/LearningDesignEditServiceSpec.js
+++ test/client/unit/LearningDesignEditServiceSpec.js
@@ -25,11 +25,33 @@ describe('Learning Design Edit Service', function() {
     		{"id":6,"name":"C2"}
     	];
 
-    	it('Finds matches', function() {
-    		var ldQcers = [{"qcer_name": "A1"}, {"qcer_name": "A2"}];
+    	it('Maps multiple selected qcer names to their corresponding qcer IDs', function() {
+    		var ldQcers = [{"qcer_name": "A1"}, {"qcer_name": "C1"}, {"qcer_name": "B2"}];
     		var results = service.generateSelectedQcers(ldQcers, qcerOpts);
     		expect(results[1]).toBe(true);
-    		expect(results[2]).toBe(true);
+    		expect(results[2]).toBe(undefined);
+    		expect(results[3]).toBe(undefined);
+    		expect(results[4]).toBe(true);
+    		expect(results[5]).toBe(true);
+    		expect(results[6]).toBe(undefined);
+    	});
+
+    	it('Maps a single selected qcer name to its corresponding qcer ID', function() {
+    		var ldQcers = [{"qcer_name": "C2"}];
+    		var results = service.generateSelectedQcers(ldQcers, qcerOpts);
+    		expect(results[1]).toBe(undefined);
+    		expect(results[2]).toBe(undefined);
+    		expect(results[3]).toBe(undefined);
+    		expect(results[4]).toBe(undefined);
+    		expect(results[5]).toBe(undefined);
+    		expect(results[6]).toBe(true);
+    	});
+
+    	it('Handles LD with no selected qcers', function() {
+    		var ldQcers = [];
+    		var results = service.generateSelectedQcers(ldQcers, qcerOpts);
+    		expect(results[1]).toBe(undefined);
+    		expect(results[2]).toBe(undefined);
     		expect(results[3]).toBe(undefined);
     		expect(results[4]).toBe(undefined);
     		expect(results[5]).toBe(undefined);
