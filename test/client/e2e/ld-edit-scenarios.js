307b8383568add6e2e8648b1851ed8bdac89a1d8
Daniela Baron <baron.daniela@gmail.com>
2013-10-20 10:10:59 -0400

    #28 enhance ld edit e2e to verify scope typeahead and public-private switch

--- test/client/e2e/ld-edit-scenarios.js
+++ test/client/e2e/ld-edit-scenarios.js
@@ -4,11 +4,18 @@
 
 describe('Edit Learning Design', function() {
 
-	it('Updates Qcers', function() {
+	it('Updates are saved', function() {
+
+		var verifyTypeAheadValues = function(cssSelector, message, expectedValues) {
+			for (var i=0; i<expectedValues.length; i++) {
+				expect(repeater(cssSelector, message).row(expectedValues[i].rowNum)).toEqual([expectedValues[i].value]);
+			};
+		};
 
 		var marioUser = testUsers.getMarioUserName;
    	 	var marioPswd = testUsers.getMarioUserPassword;
    	 	var privateLdOwnedByMario = 18;
+   	 	var scopeTypeaheadMatch = "Lesson";
 
    	 	// Login
    	 	browser().navigateTo('/login');
@@ -32,18 +39,51 @@ describe('Edit Learning Design', function() {
         expect(repeater('.prerequisites li').count()).toBe(0);
         expect(input('learningDesign.ld_students_profile').val()).toBe('20 studenti adolescenti di livello B1');
 
-        // TODO Figure out how to verify checkboxes in Angular scenario
-        expect(input('ldPublicationFlag').val()).toBe('on'); 
-        expect(input('selectedQcers[qceropt.id]').val()).toBe('on'); 
-
-        // edit qcers - click on second one
+        // Select second Qcer
         element('.titleQcer input:eq(1)').click();
-        // TODO: Figure out how to verify update was successful
+
+        // Verify Scope Typeahead
+        expect(element('#scopeFormSection .typeahead', 'scope typeahead hidden').css('display')).toBe('none');
+        input('learningDesign.ld_scope').enter('L');
+        sleep(0.5);
+        expect(element('#scopeFormSection .typeahead', 'scope typeahead is displayed').css('display')).toBe('block');
+        verifyTypeAheadValues('#scopeFormSection .ng-scope', 'scope typeahead', [
+        	{rowNum: 0, value: "<strong>L</strong>esson"}, 
+        	{rowNum: 1, value: "<strong>L</strong>ezione"}, 
+        	{rowNum: 2, value: "Modu<strong>l</strong>e"}
+        ]);
+        element('#scopeFormSection li a').click(); 
+        expect(input('learningDesign.ld_scope').val()).toBe(scopeTypeaheadMatch);
+
+        // TODO: Edit Topic and other typeaheads
+
+        // Make this LD public 
+        input('ldPublicationFlag').check();
+
+        // Verify this modified LD is first result (scope is still Module because blur event not triggered)
+        element('#homeLink').click();
+        sleep(0.5);
+        expect(browser().location().url()).toBe('/'); 
+        expect(repeater('.ld-border').row(0)).toEqual(["Learning Design Title Demo 18","A1","A2","Module","Mario","Rossi"]);
+
+        // TODO Click on ld, verify we're on view page and verify all other modified data
+
+        // Go back to Edit Page and make LD Private
+        browser().navigateTo('/ldedit/' + privateLdOwnedByMario);
+        sleep(0.5);
+        expect(browser().location().url()).toBe('/ldedit/' + privateLdOwnedByMario);
+        input('ldPublicationFlag').check();
+
+        // Verify it no longer appears on homepage
+        element('#homeLink').click();
+        sleep(0.5);
+        expect(browser().location().url()).toBe('/'); // This might not work on Travis?
+        var expectedLd1Data = ["Learningà Designè Titleì Demoò 1ù é","A1","A2","Lesson","Mario","Rossi"];
+        expect(repeater('.ld-border').row(0)).toEqual(expectedLd1Data);
 
          // Logout
         element('#userActionsMenu').click();
         element('#logoutLink').click();
-
 	});
 
 });
