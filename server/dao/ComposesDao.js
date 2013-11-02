211feb8756f931c51f845c36bdc645e2503812d1
Daniela Baron <baron.daniela@gmail.com>
2013-11-02 16:42:42 -0400

    #43 composes dao multi update

--- server/dao/ComposesDao.js
+++ server/dao/ComposesDao.js
@@ -2,11 +2,6 @@ var mysql = require('mysql')
 var dao = require('./Dao.js');
 
 var FIND_ALL_COMPOSES = 'SELECT id, ld_id, activity_id, ld_part_id, activity_group_id, level, position FROM composes where ld_id = ? ORDER BY level, position';
-
-var FIND_COMPOSES_ACTIVITY = 'SELECT id FROM composes WHERE ld_id = ? and activity_id = ? and level = ? and position = ?';
-var FIND_COMPOSES_LD = 'SELECT id FROM composes WHERE ld_id = ? and ld_part_id = ? and level = ? and position = ?';
-var FIND_COMPOSES_GROUP = 'SELECT id FROM composes WHERE ld_id = ? and activity_group_id = ? and level = ? and position = ?';
-
 var UPDATE_COMPOSES = 'UPDATE composes set level = ?, position = ? WHERE id = ?';
 
 // TODO: Same method in LdEditDao, refactor to Dao.js to make it common
@@ -26,43 +21,25 @@ module.exports = {
 		});
 	},
 
-	findComposesActivity: function(criteria, callback) {
-		dao.findAll(FIND_COMPOSES_ACTIVITY, criteria, function(err, result) {
-			handleResult(callback, err, result);
+	updateComposesMulti: function(nodes, callback) {
+		var composeInputs = module.exports.generateComposeUpdates(nodes);
+		dao.multiStatement(composeInputs.concatStatement, composeInputs.params, function(err, results) {
+			handleResult(callback, err, results);
 		});
 	},
 
-	findComposesLd: function(criteria, callback) {
-		dao.findAll(FIND_COMPOSES_LD, criteria, function(err, result) {
-			handleResult(callback, err, result);
-		});
-	},
-
-	findComposesGroup: function(criteria, callback) {
-		dao.findAll(FIND_COMPOSES_GROUP, criteria, function(err, result) {
-			handleResult(callback, err, result);
-		});
-	},
-
-	updateComposes: function(criteria, callback) {
-		dao.insertOrUpdateRecord(UPDATE_COMPOSES, criteria, function(err, result) {
-			handleResult(callback, err, result);
-		});
-	},
-
-	// #43 wip...
-	generateUpdates: function(nodes) {
+	generateComposeUpdates: function(nodes) {
 		var statementArray = [];
 		var paramArray = [];
         for (var i=0; i<nodes.length; i++) {
             statementArray.push(UPDATE_COMPOSES);
-            paramArray.push(node[i].level);
-            paramArray.push(node[i].position);
-            paramArray.push(node[i].id);
+            paramArray.push(nodes[i].level);
+            paramArray.push(nodes[i].position);
+            paramArray.push(nodes[i].id);
         };
         var statements = statementArray.join("; ");
         var result = {
-        	statements: statements,
+        	concatStatement: statements,
         	params: paramArray
         };
         return result;
