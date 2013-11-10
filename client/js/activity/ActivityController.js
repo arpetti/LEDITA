e606a93695cf28751e36f133a4a79c84e4e0e1d2
Daniela Baron <baron.daniela@gmail.com>
2013-11-09 21:05:07 -0500

    #34 hook up technology typeahead for  add activity modal

--- client/js/activity/ActivityController.js
+++ client/js/activity/ActivityController.js
@@ -1,6 +1,41 @@
 angular.module('ledita-app')
 .controller('ActCreateCtrl',
-['$scope', function($scope) {
+['$scope', 'TypeaheadHelper', function($scope, TypeaheadHelper) {
+
+	$scope.selectedTechnologies = [];
     
+    $scope.getTechnologies = function(technology) {
+    	return TypeaheadHelper.getTechnologies(technology);
+  	};
+
+  	var addTechnology = function(technology) {
+  		$scope.selectedTechnologies.push(technology);
+  	};
+
+  	var clearCurrentTechnology = function() {
+  		$scope.technology = " ";
+  	};
+
+  	$scope.addTechnologyFromSuggestion = function() {
+  		addTechnology($scope.technology);
+  		clearCurrentTechnology();
+  	};
+
+  	$scope.addTechnologyFromUserInput = function($event) {
+        $event.preventDefault();
+        addTechnology($scope.technology);
+        clearCurrentTechnology();
+    };
+
+    var removeItem = function(item, list) {
+		var index = list.indexOf(item);
+    	if(index >-1){
+    		list.splice(index,1);
+		}
+    };
+
+    $scope.removeTechnology = function(technology) {
+    	removeItem(technology, $scope.selectedTechnologies);
+    };
 
 }]);
