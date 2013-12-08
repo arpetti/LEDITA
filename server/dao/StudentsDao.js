ad1e24a982c3399f6ee6502271c5e733ef7c95bd
Daniela Baron <baron.daniela@gmail.com>
2013-12-07 20:48:41 -0500

    students dao - insert students

--- server/dao/StudentsDao.js
+++ server/dao/StudentsDao.js
@@ -1,6 +1,7 @@
 var dao = require('./Dao');
 
 var FIND_STUDENTS_TYPE = 'select type, description from students_type where type = ?';
+var INSERT_STUDENTS = 'insert into students set ?';
 
 var handleResult = function(cb, err, results) {
 	if(err) {
@@ -16,6 +17,12 @@ module.exports = {
 		dao.findAll(FIND_STUDENTS_TYPE, [type], function(err, results) {
       handleResult(cb, err, results);
     });
+	},
+
+	insertStudents: function(studentsObj, cb) {
+		dao.insertOrUpdateRecord(INSERT_STUDENTS, studentsObj, function(err, insertId) {
+			handleResult(cb, err, insertId);
+		});
 	}
 
 }; 
--- test/server/dao/StudentsDaoSpec.js
+++ test/server/dao/StudentsDaoSpec.js
@@ -1,6 +1,7 @@
 var chai = require('chai');
 var expect = require('chai').expect;
 var fixture = require('../../../server/dao/StudentsDao');
+var dao = require('../../../server/dao/Dao');
 
 describe('Students DAO', function() {
 
@@ -28,6 +29,51 @@ describe('Students DAO', function() {
 		})
 
 	});
-	
+
+	describe('Insert Students', function() {
+
+		var cleanupQuery = 'delete from students where id = ?';
+
+		it('Inserts a new Students record', function(done) {
+			var studentsObj = { type: '2' };
+			fixture.insertStudents(studentsObj, function(err, insertId) {
+				expect(err).to.be.null;
+				expect(insertId).not.to.be.null;
+				expect(insertId).to.be.above(0);
+				dao.deleteRecord(cleanupQuery, [insertId], function(err, result) {
+					expect(err).to.be.null;
+					done();
+				});
+			});
+		});
+
+		it('Inserts a new Student record with group details', function(done) {
+			var studentsObj = {
+				type: '4',
+				group_number: 2,
+				people_per_group: 6
+			};
+			fixture.insertStudents(studentsObj, function(err, insertId) {
+				expect(err).to.be.null;
+				expect(insertId).not.to.be.null;
+				expect(insertId).to.be.above(0);
+				dao.deleteRecord(cleanupQuery, [insertId], function(err, result) {
+					expect(err).to.be.null;
+					done();
+				});
+			});
+		});
+
+		it('Calls back with error for non existing type', function(done) {
+			var studentsObj = { type: '999' };
+			fixture.insertStudents(studentsObj, function(err, insertId) {
+				expect(err).not.to.be.null;
+				expect(err.message).to.contain('fk_students_type');
+				expect(insertId).to.be.undefined;
+				done();
+			});
+		});
+
+	});
 
 });
