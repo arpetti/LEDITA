8f25c6e84135a083bc5f454352a55dec777d24d1
Daniela Baron <baron.daniela@gmail.com>
2014-01-05 17:39:01 -0500

    #51 dao for public and private projects, and fix demo data for ld missing qcers

--- server/dao/UserProjectsDao.js
+++ server/dao/UserProjectsDao.js
@@ -1,21 +1,26 @@
 var mysql = require('mysql');
 var dao = require('./Dao');
 
-var GET_PUBLIC_PROJECTS = 'select ld_id, ld_name, ld_scope, ld_creation_date, user_id, ld_publication, ' +
+var PUBLIC_LD = [1];
+var PUBLIC_AND_PRIVATE_LD = [0, 1];
+
+var GET_PROJECTS = 'select ld_id, ld_name, ld_scope, ld_creation_date, user_id, ld_publication, ' +
 	'group_concat(qcer_name order by qcer_name ASC separator \' \') as ld_qcers from vw_ld_user_qcer ' +
 	'group by ld_id, ld_name, ld_scope, ld_creation_date, user_id, ld_publication ' +
-	'having user_id = ? and ld_publication = 1 ' +
+	'having user_id = ? and ld_publication in (?) ' +
 	'order by ld_creation_date desc';
 
 module.exports = {
 
 	getPublicProjects: function(userId, callback) {
-		dao.findAll(GET_PUBLIC_PROJECTS, [userId], function(err, results) {
-			if(err){
-				callback(err);
-			} else {
-				callback(null, results);
-			}
+		dao.findAll(GET_PROJECTS, [userId, PUBLIC_LD], function(err, results) {
+			dao.handleResult(callback, err, results);
+		});
+	},
+
+	getPublicAndPrivateProjects: function(userId, callback) {
+		dao.findAll(GET_PROJECTS, [userId, PUBLIC_AND_PRIVATE_LD], function(err, results) {
+			dao.handleResult(callback, err, results);
 		});
 	}
 
--- test/server/dao/UserProjectsDaoSpec.js
+++ test/server/dao/UserProjectsDaoSpec.js
@@ -19,11 +19,35 @@ describe('User Projects DAO', function() {
 		var userId = 1;
 		fixture.getPublicProjects(userId, function(err, results) {
 			expect(err).to.be.null;
-			expect(results).to.have.length(3);
+			expect(results).to.have.length(4);
 
 			verifyProject(results[0], 'Learningà Designè Titleì Demoò 1ù é', 'Lesson', LD_PUBLIC, 'A1 A2', userId);
 			verifyProject(results[1], 'Learning Design Title Demo 9', 'Lesson', LD_PUBLIC, 'A1', userId);
 			verifyProject(results[2], 'Learning Design Title Demo 17', 'Lesson', LD_PUBLIC, 'C1', userId);
+
+			var sortedResults = _.sortBy(results, function(obj) {return obj.creation_date;});
+			for(var i=0; i<results.length; i++) {
+				expect(_.isEqual(results[i], sortedResults[i])).to.be.true;
+			}
+
+			done();
+		});
+	});
+
+	it('Gets public and private projects for user sorted by desceding creation date with qcers collapsed in a single row', function(done) {
+		var userId = 1;
+		fixture.getPublicAndPrivateProjects(userId, function(err, results) {
+			expect(err).to.be.null;
+			expect(results).to.have.length(8);
+			
+			verifyProject(results[0], 'Learningà Designè Titleì Demoò 1ù é', 'Lesson', LD_PUBLIC, 'A1 A2', userId);
+			verifyProject(results[1], 'Learning Design Title Demo 2', 'Module', LD_PRIVATE, 'B1', userId);
+			verifyProject(results[2], 'Learning Design Title Demo 9', 'Lesson', LD_PUBLIC, 'A1', userId);
+			verifyProject(results[3], 'Learning Design Title Demo 10', 'Module', LD_PRIVATE, 'A1', userId);
+			verifyProject(results[4], 'Learning Design Title Demo 17', 'Lesson', LD_PUBLIC, 'C1', userId);
+			verifyProject(results[5], 'Learning Design Title Demo 18', 'Module', LD_PRIVATE, 'A1', userId);
+			verifyProject(results[6], 'Learning Design Title Demo 25', 'Lesson', LD_PUBLIC, 'A2 B2', userId);
+			verifyProject(results[7], 'Learning Design Title Demo 26', 'Module', LD_PRIVATE, 'C2', userId);
 
 			var sortedResults = _.sortBy(results, function(obj) {return obj.creation_date;});
 			for(var i=0; i<results.length; i++) {
