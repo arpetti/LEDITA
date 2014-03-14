9f3bbdea231ad00967b3f6ab9769d38e6976308b
Alessandro <arpetti@gmail.com>
2014-03-14 13:48:58 -0300

    Delete activity from 'composes'

--- server/controllers/ActivityEditController.js
+++ server/controllers/ActivityEditController.js
@@ -1,11 +1,24 @@
+var ActivityEditService = require('../service/ActivityEditService');
+
+
 // #57 wip
 module.exports = {
 
 	deleteActivity: function(req, res) {
 		var activityId = req.params.actid;
-		console.log('ActivityEditController.deleteActivity: activityId = ' + activityId);
+        var ldId = req.params.id;
+
+        ActivityEditService.deleteActivity(ldId, activityId, function(err, successInfo, message){
+            if (err) {
+                res.send(400, message);
+            } else {
+                res.send(200);
+            }}
+            );
+
+		//console.log('ActivityEditController.deleteActivity: activityId = ' + activityId);
 		//TODO: call service to delete activity
-		res.send(200);
+		//res.send(200);
 	}
 
 };
--- server/service/ActivityEditService.js
+++ server/service/ActivityEditService.js
@@ -1,4 +1,8 @@
 var activityEditDao = require('../dao/ActivityEditDao');
+var mysql = require('mysql')
+var dao = require('../dao/Dao.js');
+
+var DELETE_ACTIVITY = 'DELETE FROM composes WHERE ld_id = ? AND activity_id = ?';
 
 module.exports = {
 	
@@ -10,6 +14,11 @@ module.exports = {
 				callback(true);
 			}
 		});
-	}
+	},
 
-}; 
+    deleteActivity: function(ldId, activityId, callback){
+        dao.deleteRecord(DELETE_ACTIVITY, [ldId, activityId], function(err, result) {
+            dao.handleResult(callback, err, result);
+        });
+
+}}
